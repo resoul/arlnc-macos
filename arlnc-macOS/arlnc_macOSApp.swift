595806324d2fc8f9a205c55e43483ff11ff4c0d7
resoul <resoul.ua@icloud.com>
2024-03-02 13:42:19 +0200

    Initial Commit

---
+++ arlnc-macOS/arlnc_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  arlnc_macOSApp.swift
+//  arlnc-macOS
+//
+//  Created by resoul on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct arlnc_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
